24268ea10be3ea6069a4e4d67fac54e909e6ad3e
pasoldne <vitorpasold4@gmail.com>
2026-09-12 00:43:50 -0300

    service feito, kmPorLitro e custoPorKm

---
+++ src/main/java/com/pasold/kombi/services/AbastecimentoService.java
@@ -0,0 +1,31 @@
+package com.pasold.kombi.services;
+import com.pasold.kombi.dtos.EntradaAbastecimentoDto;
+import com.pasold.kombi.dtos.RetornoAbastecimentoDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AbastecimentoService {
+
+    public AbastecimentoService() {
+    }
+
+    public RetornoAbastecimentoDto calculo (EntradaAbastecimentoDto entrada){
+        RetornoAbastecimentoDto saida = new RetornoAbastecimentoDto();
+        saida.setCombustivel(entrada.getCombustivel());
+        saida.setCustoPorKm(custoPorKm(entrada));
+        saida.setKmPorLitro(kmPorLitro(entrada));
+        saida.setLitrosAbastecidos(entrada.getLitrosAbastecidos());
+        saida.setModelo(entrada.getModelo());
+        saida.setQuilometragem(entrada.getQuilometragem());
+        saida.setValorPago(entrada.getValorPago());
+        return saida;
+    }
+
+    private Double kmPorLitro(EntradaAbastecimentoDto entrada){
+        return entrada.getQuilometragem()/entrada.getLitrosAbastecidos();
+    }
+
+    private Double custoPorKm(EntradaAbastecimentoDto entrada){
+        return kmPorLitro(entrada)*6.56D;
+    }
+}
