8587dee8a29a1e5e58ace2095af1bcb9746bd306
pasoldne <vitorpasold4@gmail.com>
2026-09-11 22:35:05 -0300

    first commit

---
+++ src/main/java/com/pasold/kombi/KombiApplication.java
@@ -0,0 +1,13 @@
+package com.pasold.kombi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KombiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KombiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasold/kombi/KombiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasold.kombi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KombiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
